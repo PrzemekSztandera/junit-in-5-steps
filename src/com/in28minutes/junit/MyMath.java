f51a1468890074ce169f5da5a8122706ff4eac93
sztanderap <sztandera.p@gmail.com>
2019-10-22 16:28:24 +0100

    Added MyMath.java and MyMathTest.java

--- src/com/in28minutes/junit/MyMath.java
+++ src/com/in28minutes/junit/MyMath.java
@@ -10,4 +10,8 @@ public class MyMath {
         }
         return sum;
     }
+
+    public static void main(String[] args) {
+
+    }
 }
--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -1,5 +1,11 @@
 package com.in28minutes.junit;
 
+import org.junit.Test;
+
 public class MyMathTest {
 
+    @Test
+    public void test() {
+
+    }
 }
